fa6049a8d8a5ee2eb78d40b7b779d92e28a8d4f7
C-Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-12-27 13:19:28 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 cd
 rm -rf Password_Generator
 cd
-git clone https://github.com/C-Joydip/Password_Generator
+git clone https://github.com/C-Joydip/Password-Generator
 cd Password_Generator
-echo "Your Password_Generator tool has been updated" | lolcat -as 10
+echo "Your Password-Generator tool has been updated" | lolcat -as 10
